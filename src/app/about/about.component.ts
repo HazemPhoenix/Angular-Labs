f783ec64a17461a5c5a9fbb327107e79b410f006
hazemphoenix <hazem.mando408@gmail.com>
2025-05-29 16:32:38 +0300

    feat: implement the CV download button

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -4,8 +4,14 @@ import { Component } from '@angular/core';
   selector: 'app-about',
   imports: [],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrl: './about.component.css',
 })
 export class AboutComponent {
-
+  downloadCV() {
+    const link = document.createElement('a');
+    link.href = '/Profile.pdf';
+    link.download = 'Hazem Abdulmoneim - CV.pdf';
+    link.click();
+    link.remove();
+  }
 }
