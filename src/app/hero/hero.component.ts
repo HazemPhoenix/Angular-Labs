b7f8522e22832d15d873d014d81c6ebdda7b8fb1
hazemphoenix <hazem.mando408@gmail.com>
2025-05-29 19:55:08 +0300

    feat: connect the buttons in the hero section to their respective sections

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -4,8 +4,26 @@ import { Component } from '@angular/core';
   selector: 'app-hero',
   imports: [],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+  styleUrl: './hero.component.css',
 })
 export class HeroComponent {
+  scrollToPortfolio() {
+    const portfolioSection = document.getElementById('portfolio');
+    if (portfolioSection) {
+      portfolioSection.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
+  }
 
+  scrollToContact() {
+    const contactSection = document.getElementById('contact');
+    if (contactSection) {
+      contactSection.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
+  }
 }
